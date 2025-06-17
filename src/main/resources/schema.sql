484d3996ae5fdece1bc420a64150d20a8e4527d8
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2025-06-17 13:45:10 +0200

    smth

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+create  table  animals(
+            id bigint not null,
+            name varchar(30),
+            binomial_name varchar(50),
+            class varchar(50),
+            family var
+
+)
