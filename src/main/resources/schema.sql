dd37ee1f4a9a896576b58c82984e359d19210e85
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2025-06-17 23:52:37 +0200

    format schema SQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,49 +7,28 @@ create table species
     family        varchar(255),
     partition_key varchar(255),
     PRIMARY KEY (id, partition_key)
-) PARTITION BY LIST (partition_key)
+) PARTITION BY LIST (partition_key);
 
 CREATE TABLE species_asia
-    PARTITION OF species FOR VALUES IN
-(
-    'Asia'
-)
+    PARTITION OF species FOR VALUES IN ('Asia');
 
 CREATE TABLE species_africa
-    PARTITION OF species FOR VALUES IN
-(
-    'Africa'
-)
+    PARTITION OF species FOR VALUES IN ('Africa');
 
 CREATE TABLE species_north_america
-    PARTITION OF species FOR VALUES IN
-(
-    'North America'
-)
+    PARTITION OF species FOR VALUES IN ('North America');
 
 CREATE TABLE species_south_america
-    PARTITION OF species FOR VALUES IN
-(
-    'South America'
-)
+    PARTITION OF species FOR VALUES IN ('South America');
 
 CREATE TABLE species_europe
-    PARTITION OF species FOR VALUES IN
-(
-    'Europe'
-)
+    PARTITION OF species FOR VALUES IN ('Europe');
 
 CREATE TABLE species_australia
-    PARTITION OF species FOR VALUES IN
-(
-    'Australia'
-)
+    PARTITION OF species FOR VALUES IN ('Australia');
 
 CREATE TABLE species_antarctica
-    PARTITION OF species FOR VALUES IN
-(
-    'Antarctica'
-)
+    PARTITION OF species FOR VALUES IN ('Antarctica');
 
 CREATE TABLE pets
 (
@@ -60,32 +39,32 @@ CREATE TABLE pets
     specie_id     bigint,
     partition_key varchar(255),
     PRIMARY KEY (id, partition_key)
-) PARTITION BY LIST (partition_key)
+) PARTITION BY LIST (partition_key);
 
 CREATE TABLE pets_asia
-    PARTITION OF pets FOR VALUES IN ('Asia')
+    PARTITION OF pets FOR VALUES IN ('Asia');
 
 CREATE TABLE pets_africa
-    PARTITION OF pets FOR VALUES IN ('Africa')
+    PARTITION OF pets FOR VALUES IN ('Africa');
 
 CREATE TABLE pets_north_america
-    PARTITION OF pets FOR VALUES IN ('North America')
+    PARTITION OF pets FOR VALUES IN ('North America');
 
 CREATE TABLE pets_south_america
-    PARTITION OF pets FOR VALUES IN ('South America')
+    PARTITION OF pets FOR VALUES IN ('South America');
 
 CREATE TABLE pets_europe
-    PARTITION OF pets FOR VALUES IN ('Europe')
+    PARTITION OF pets FOR VALUES IN ('Europe');
 
 CREATE TABLE pets_australia
-    PARTITION OF pets FOR VALUES IN ('Australia')
+    PARTITION OF pets FOR VALUES IN ('Australia');
 CREATE TABLE pets_antarctica
-    PARTITION OF pets FOR VALUES IN ('Antarctica')
+    PARTITION OF pets FOR VALUES IN ('Antarctica');
 
 ALTER TABLE IF EXISTS pets
     ADD CONSTRAINT fk_pets_specie_id
     FOREIGN KEY (specie_id, partition_key)
-    REFERENCES species
+    REFERENCES species;
 
 
 
