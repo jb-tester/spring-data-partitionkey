f3abfa7b4141125d6d891edad2eaf9ec17071a19
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2025-06-17 13:39:20 +0200

    Initial commit

---
+++ src/main/java/org/example/springdatapartitionkey/SpringDataPartitionkeyApplication.java
@@ -0,0 +1,13 @@
+package org.example.springdatapartitionkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataPartitionkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataPartitionkeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springdatapartitionkey/SpringDataPartitionkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springdatapartitionkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataPartitionkeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
